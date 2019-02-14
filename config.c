56709facc3ec39fb3467ca9e222ff9cf73fb6b0a
hx1997 <hx970923@gmail.com>
2019-02-14 12:45:22 +0800

    add usage message

--- config.c
+++ config.c
@@ -9,9 +9,24 @@
 
 DisassemblerConfig cf = {0};
 
+static void usage(const char *executable) {
+    printf("Usage: %s -s SIZE [-a ADDR] [-b BASE] [-h] FILE\n", executable);
+    printf("-s SIZE\t\tdisassemble SIZE bytes (in decimal) starting from ADDR\n");
+    printf("-a ADDR\t\tstart disassembling from file offset ADDR (in hex)\n");
+    printf("-b BASE\t\tspecify the image base in hex (i.e. the address an executable is loaded to in memory); "
+           "the disassember will add BASE to file offsets to form virtual addresses (VA). "
+           "Note: this BASE is calculated automatically if FILE is a PE; you don't need to specify it manually.\n");
+    printf("-h\t\tdisplay this help message\n");
+}
+
 void conf_parse_args(int argc, char **argv) {
+    if (argc < 2) {
+        usage(argv[0]);
+        exit(-1);
+    }
+
     // loop through argv
-    for (int i = 0; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-m16") == 0) {
             cf.mode_bitwidth = BIT_WIDTH_16;
         } else if (strcmp(argv[i], "-m32") == 0) {
@@ -20,16 +35,24 @@ void conf_parse_args(int argc, char **argv) {
             cf.mode_bitwidth = BIT_WIDTH_64;
         } else if (strcmp(argv[i], "-s") == 0) {
             cf.size_to_disasm = (unsigned int)strtol(argv[i+1], 0, 10);
+            i++;
         } else if (strcmp(argv[i], "-a") == 0) {
             cf.start_address = strtoul(argv[i+1], 0, 16);
+            i++;
         } else if (strcmp(argv[i], "-b") == 0) {
             cf.image_base = strtoul(argv[i+1], 0, 16);
+            i++;
+        } else if (strcmp(argv[i], "-h") == 0) {
+            usage(argv[0]);
+            exit(0);
         } else if (*argv[i] == '-') {
             fprintf(stderr, "conf_parse_args(): ignoring invalid argument '%s'!\n", argv[i]);
+            usage(argv[0]);
         } else {
             if (cf.size_to_disasm == 0) {
                 fprintf(stderr, "conf_parse_args(): disassembly of PE files requires the '-s' option! "
-                                "Either you did not specify it, or the argument specified is invalid!");
+                                "Either you did not specify it, or the argument specified is invalid!\n");
+                usage(argv[0]);
                 exit(-1);
             }
             cf.disasm_file = argv[i];
--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 int main(int argc, char **argv) {
     cf.mode_bitwidth = BIT_WIDTH_32;
 
-    if (argc > 1) {
-        conf_parse_args(argc-1, argv+1);    /* skip the executable name */
-    }
+    conf_parse_args(argc, argv);
 
     disasm_pe_file(cf.disasm_file, cf.size_to_disasm, cf.start_address);
     return 0;
